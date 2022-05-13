aec846b19d9f06cc99979d91fb9659db801ef91e
kxngj <jaheimcbrown@gmail.com>
2022-05-13 13:12:10 -0500

    initial commit

---
+++ controllers/tasks.js
@@ -0,0 +1,19 @@
+const getAllItems = (req, res) => {
+  res.send("Get all tasks.");
+};
+
+const createTask = (req, res) => {
+  res.json(req.body);
+};
+
+const getTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+const updateTask = (req, res) => {
+  res.send("Update task.");
+};
+const deleteTask = (req, res) => {
+  res.send("Delete task.");
+};
+
+module.exports = { getAllItems, createTask, getTask, updateTask, deleteTask };
